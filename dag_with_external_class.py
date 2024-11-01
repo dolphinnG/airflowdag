f72a19e3727c7a7105e265d31e70c9d57b1dc0ef
r-RealG <justmindgame1@gmail.com>
2024-11-01 19:33:40 +0700

    test dag with external class

---
+++ dag_with_external_class.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+from airflow.decorators import task
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+dag = DAG(
+    'simple_dag',
+    default_args=default_args,
+    description='A simple DAG',
+    schedule_interval=None,
+)
+
+start = EmptyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+@task
+def process_data():
+    print("Processing data...")
+
+end = process_data()
+
+start >> end
