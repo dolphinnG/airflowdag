a2a35803e905fb7123dfa2bfa441841503ff208d
r-RealG <justmindgame1@gmail.com>
2024-11-02 04:22:10 +0700

    sdsd

--- test_relative_import.py
+++ test_relative_import.py
@@ -1,33 +1,33 @@
-from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.operators.empty import EmptyOperator
-from airflow.decorators import task
-from .test.A import Aclass #NOPE
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-}
+# from datetime import datetime, timedelta
+# from airflow import DAG
+# from airflow.operators.empty import EmptyOperator
+# from airflow.decorators import task
+# from .test.A import Aclass #NOPE
+# default_args = {
+#     'owner': 'airflow',
+#     'depends_on_past': False,
+#     'email_on_failure': False,
+#     'email_on_retry': False,
+#     'retries': 1,
+# }
 
-dag = DAG(
-    'simple_ddsfasdfsdafsadfag2',
-    default_args=default_args,
-    description='A simple DAG',
-    schedule_interval=None,
-)
+# dag = DAG(
+#     'simple_ddsfasdfsdafsadfag2',
+#     default_args=default_args,
+#     description='A simple DAG',
+#     schedule_interval=None,
+# )
 
-start = EmptyOperator(
-    task_id='start',
-    dag=dag,
-)
+# start = EmptyOperator(
+#     task_id='start',
+#     dag=dag,
+# )
 
-@task
-def process_data():
-    print("Processinddddddddddddddg data...")
-    Aclass()
+# @task
+# def process_data():
+#     print("Processinddddddddddddddg data...")
+#     Aclass()
 
-end = process_data()
+# end = process_data()
 
-start >> end
+# start >> end
